e8483c19e5b9fdc9fed4c7d916f4bc5a3aa69a8e
Mathias <mathiassolar24@gmail.com>
2024-04-24 02:04:42 -0400

    Edicion de personajes

--- lib/pages/tabs/tabperfil.dart
+++ lib/pages/tabs/tabperfil.dart
@@ -45,96 +45,171 @@ class TabPerfil extends StatelessWidget {
 
 Widget _buildTheShinobiContent() {
   return Container(
-    color: Colors.black, 
+    color: Colors.black,
+    padding: EdgeInsets.all(20.0),
     child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Image.asset('assets/images/shinobiicon.png',height: 500, width: 500),
-        Text(
-          'Información sobre The Shinobi',
-          style: TextStyle(
-            color: Colors.white, 
-            fontSize: 20,
-          ),
-        ),
+        SizedBox(height: 300, width: 500,
+          child: Image.asset('assets/images/shinobiicon.png')),
+        SizedBox(height: 20.0),
+        Text('Información sobre The Shinobi', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20.0),
+        Text('Nacionalidad: Indonesia', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Ocupación: Asesino profesional', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Afiliación: La Alta Mesa', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Asesinatos: 10', style: TextStyle(color: Colors.white, fontSize: 18)),
       ],
     ),
   );
 }
 
 
-  Widget _buildZeroContent() {
-    return Column(
-      
-      mainAxisAlignment: MainAxisAlignment.center,
+Widget _buildZeroContent() {
+ return Container(
+    color: Colors.black,
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Image.asset('assets/images/zeroicon.png',height: 500, width: 500),
-        Text('Información sobre Zero'),
+        SizedBox(height: 300, width: 500,
+          child: Image.asset('assets/images/zeroicon.png')),
+        SizedBox(height: 20.0),
+        Text('Información sobre Zero', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20.0),
+        Text('Nacionalidad: Japones', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Ocupación: Asesino profesional', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Afiliación: La Alta Mesa', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Asesinatos: 5', style: TextStyle(color: Colors.white, fontSize: 18)),
       ],
+    ),
+  );
+}
+
+Widget _buildChidiContent() {
+   return Container(
+    color: Colors.black,
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 300, width: 500,
+          child: Image.asset('assets/images/chidiicon.png')),
+        SizedBox(height: 20.0),
+        Text('Información sobre Chidi', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20.0),
+        Text('Nacionalidad: Chileno', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Ocupación: Líder de los mirmidones Jefe de guardaespaldas y ejecutor del marqués Vincent de Gramont', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Afiliación: La Mesa Alta, Los Mirmidones Marqués Vincent de Gramont', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Asesinatos: 1', style: TextStyle(color: Colors.white, fontSize: 18)),
+      ],
+    ),
+  );
+}
+
+Widget _buildCaineContent() {
+   return Container(
+    color: Colors.black,
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 300, width: 500,
+          child: Image.asset('assets/images/caineicon.png')),
+        SizedBox(height: 20.0),
+        Text('Información sobre Caine', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20.0),
+        Text('Nacionalidad: hongkonés', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Ocupación: Asesino profesional', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Afiliación: La Alta Mesa', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Asesinatos: 6', style: TextStyle(color: Colors.white, fontSize: 18)),
+      ],
+    ),
+  );
+}
+
+Widget _buildKillaContent() {
+  return Container(
+      color: Colors.black,
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 300, width: 500,
+            child: Image.asset('assets/images/killaicon.png')),
+          SizedBox(height: 20.0),
+          Text('Información sobre Killa Harkan', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+          SizedBox(height: 20.0),
+          Text('Nacionalidad: Aleman', style: TextStyle(color: Colors.white, fontSize: 18)),
+          Text('Ocupación: Propietario del club nocturno Crime Lord', style: TextStyle(color: Colors.white, fontSize: 18)),
+          Text('Afiliación: La Alta Mesa', style: TextStyle(color: Colors.white, fontSize: 18)),
+          Text('Asesinatos: 3', style: TextStyle(color: Colors.white, fontSize: 18)),
+        ],
+      ),
     );
   }
 
-  Widget _buildChidiContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+Widget _buildTheAdjudicatorContent() {
+   return Container(
+    color: Colors.black,
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Image.asset('assets/images/chidiicon.png',height: 500, width: 500),
-        Text('Información sobre Chidi'),
+        SizedBox(height: 300, width: 500,
+          child: Image.asset('assets/images/theadjudicatoricon.png')),
+        SizedBox(height: 20.0),
+        Text('Información sobre The Adjudicator', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20.0),
+        Text('Nacionalidad: Amerciano', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Ocupación: Ejecutor de la mesa alta', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Afiliación: La Alta Mesa', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Asesinatos: 15', style: TextStyle(color: Colors.white, fontSize: 18)),
       ],
-    );
-  }
+    ),
+  );
+}
 
-  Widget _buildCaineContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+Widget _buildTheHarbingerContent() {
+  return Container(
+    color: Colors.black,
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Image.asset('assets/images/caineicon.png',height: 500, width: 500),
-        Text('Información sobre Caine '),
+        SizedBox(height: 300, width: 500,
+          child: Image.asset('assets/images/theharbingericon.png')),
+        SizedBox(height: 20.0),
+        Text('Información sobre The Harbinger', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20.0),
+        Text('Nacionalidad: Amerciano', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Ocupación: Ejecutor de la mesa alta', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Afiliación: La Alta Mesa', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Asesinatos: 4', style: TextStyle(color: Colors.white, fontSize: 18)),
       ],
-    );
-  }
-
-    Widget _buildKillaContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset('assets/images/killaicon.png',height: 500, width: 500),
-        Text('Información sobre killa '),
-
-      ],
-    );
-  }
-
-  Widget _buildTheAdjudicatorContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset('assets/images/theadjudicatoricon.png',height: 500, width: 500),
-        Text('Información sobre The Adjudicator '),
-
-      ],
-    );
-  }
-
-    Widget _buildTheHarbingerContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Image.asset('assets/images/theharbingericon.png',height: 500, width: 500),
-        Text('Información sobre The Harbinger '),
-
-      ],
-    );
+    ),
+  );
   }
 
   Widget _buildVincentContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    return Container(
+    color: Colors.black,
+    padding: EdgeInsets.all(20.0),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Image.asset('assets/images/vicenticon.png',height: 500, width: 500),
-        Text('Información sobre Vincent '),
-
+        SizedBox(height: 300, width: 500,
+          child: Image.asset('assets/images/vicenticon.png')),
+        SizedBox(height: 20.0),
+        Text('Información sobre Vincent Bisset de Gramont', style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold)),
+        SizedBox(height: 20.0),
+        Text('Nacionalidad: Frances', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Ocupación: Miembro senior aristócrata de la mesa alta', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Afiliación: La Mesa Alta La familia De Gramont Los Mirmidones La Main Rouge', style: TextStyle(color: Colors.white, fontSize: 18)),
+        Text('Asesinatos: 2', style: TextStyle(color: Colors.white, fontSize: 18)),
       ],
-    );
+    ),
+  );
   }
 
 }
